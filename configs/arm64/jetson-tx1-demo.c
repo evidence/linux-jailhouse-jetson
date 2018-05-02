f116fcdec160e6c903d4ccc6207756b3e85f169b
Luca Cuomo <l.cuomo@evidence.eu.com>
2018-05-02 15:42:59 +0200

    TX1: cell configs with IVSHMEM support

--- configs/arm64/jetson-tx1-demo.c
+++ configs/arm64/jetson-tx1-demo.c
@@ -1,13 +1,14 @@
 /*
  * Jailhouse, a Linux-based partitioning hypervisor
  *
- * Configuration for gic-demo or uart-demo inmate on Nvidia Jetson TX1:
+ * Configuration for uart+ivshmem demo inmate on Nvidia Jetson TX1:
  * 1 CPU, 64K RAM, serial port 0
  *
- * Copyright (c) Siemens AG, 2015
+ * Copyright (c)  2018 Evidence Srl
  *
  * Authors:
- *  Jan Kiszka <jan.kiszka@siemens.com>
+ *  Luca Cuomo <l.cuomo@evidence.eu.com>
+ *
  *
  * This work is licensed under the terms of the GNU GPL, version 2.  See
  * the COPYING file in the top-level directory.
@@ -21,7 +22,9 @@
 struct {
 	struct jailhouse_cell_desc cell;
 	__u64 cpus[1];
-	struct jailhouse_memory mem_regions[2];
+	struct jailhouse_memory mem_regions[4];
+	struct jailhouse_irqchip irqchips[2];
+	struct jailhouse_pci_device pci_devices[2];
 } __attribute__((packed)) config = {
 	.cell = {
 		.signature = JAILHOUSE_CELL_DESC_SIGNATURE,
@@ -31,6 +34,13 @@ struct {
 
 		.cpu_set_size = sizeof(config.cpus),
 		.num_memory_regions = ARRAY_SIZE(config.mem_regions),
+		.num_irqchips = ARRAY_SIZE(config.irqchips),
+		.num_pci_devices = ARRAY_SIZE(config.pci_devices),
+		/*On Jetson TX1 the IRQs from 212 to 223 are not assigned.
+		The bare metal cell will use IRQs from 218 to 223.
+		Note: Jailhouse adds 32 (GIC's SPI) to the .vpci_irq_base,
+		so 186 is the base value*/
+		.vpci_irq_base = 186,
 	},
 
 	.cpus = {
@@ -46,11 +56,81 @@ struct {
 				JAILHOUSE_MEM_IO,
 		},
 		/* RAM */ {
-			.phys_start = 0x17bef0000,
+			.phys_start = 0x17bfe0000,
 			.virt_start = 0,
 			.size = 0x00010000,
 			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
 				JAILHOUSE_MEM_EXECUTE | JAILHOUSE_MEM_LOADABLE,
 		},
+		/* IVHSMEM  1*/ {
+                        .phys_start = 0x17ba00000,
+                        .virt_start = 0x17ba00000,
+                        .size = 0x100000,
+                        .flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE  |
+                                JAILHOUSE_MEM_ROOTSHARED,
+
+                },
+
+                /* IVHSMEM  2*/ {
+                        .phys_start = 0x17bd00000,
+                        .virt_start = 0x17bd00000,
+                        .size = 0x100000,
+                        .flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+                                JAILHOUSE_MEM_ROOTSHARED ,
+
+                },
+
 	},
+
+	.irqchips = {
+		/* GIC */ {
+			.address = 0x50041000,
+                        .pin_base = 32,
+                        /* Interrupts:
+                           46 for UART C  */
+                        .pin_bitmap = {
+				0,
+				1<<(46-32)
+                        },
+                },
+
+		/* GIC */ {
+			.address = 0x50041000,
+                        .pin_base = 160,
+                        /* Interrupts:
+                           186 for IVSHMEM,
+                           belongs to the bare metal cell  */
+                        .pin_bitmap = {
+				0,
+				3<<(186-160)
+                        },
+                },
+        },
+
+	.pci_devices = {
+                {
+                        .type = JAILHOUSE_PCI_TYPE_IVSHMEM,
+                        .bdf = 0x0 << 3,
+                        .bar_mask = {
+                                0xffffff00, 0xffffffff, 0x00000000,
+                                0x00000000, 0x00000000, 0x00000000,
+                        },
+			/* num_msix_vectors needs to be 0 for INTx operation*/
+			.num_msix_vectors = 0, 
+                        .shmem_region = 2, /* must be no of IVSHMEM region above */
+			.shmem_protocol = JAILHOUSE_SHMEM_PROTO_UNDEFINED,
+                },
+		 {
+                        .type = JAILHOUSE_PCI_TYPE_IVSHMEM,
+                        .bdf = 0xf << 3,
+                        .bar_mask = {
+                                0xffffff00, 0xffffffff, 0x00000000,
+                                0x00000000, 0x00000000, 0x00000000,
+                        },
+                        /* num_msix_vectors needs to be 0 for INTx operation*/
+                        .num_msix_vectors = 0,
+                        .shmem_region = 3, /* must be no of IVSHMEM region above */
+                        .shmem_protocol = JAILHOUSE_SHMEM_PROTO_UNDEFINED,
+                },
+        },
 };
