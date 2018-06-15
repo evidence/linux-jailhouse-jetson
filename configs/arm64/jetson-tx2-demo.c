2c8dbe56f6044f37e01d86b89dd44731e7514ab4
Luca Cuomo <l.cuomo@evidence.eu.com>
2018-06-15 16:39:18 +0200

    [TX2]: added ivshmem and pci setup on TX2 root cell and inmate cell; added unmapped memregion to root cell

--- configs/arm64/jetson-tx2-demo.c
+++ configs/arm64/jetson-tx2-demo.c
@@ -16,7 +16,9 @@
 struct {
 	struct jailhouse_cell_desc cell;
 	__u64 cpus[1];
-	struct jailhouse_memory mem_regions[3];
+	struct jailhouse_memory mem_regions[5];
+	struct jailhouse_irqchip irqchips[1];
+	struct jailhouse_pci_device pci_devices[2];
 } __attribute__((packed)) config = {
 	.cell = {
 		.signature = JAILHOUSE_CELL_DESC_SIGNATURE,
@@ -26,6 +28,11 @@ struct {
 
 		.cpu_set_size = sizeof(config.cpus),
 		.num_memory_regions = ARRAY_SIZE(config.mem_regions),
+		.num_irqchips = ARRAY_SIZE(config.irqchips),
+		.num_pci_devices = ARRAY_SIZE(config.pci_devices),
+		/* FIXME: add IRQ for pci */
+		.vpci_irq_base = 300,
+
 	},
 
 	.cpus = {
@@ -53,5 +60,68 @@ struct {
 			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
 				JAILHOUSE_MEM_COMM_REGION,
 		},
+
+		/* IVHSMEM  1*/ {
+                        .phys_start = 0x275000000,
+                        .virt_start = 0x275000000,
+                        .size = 0x1000,
+                        .flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE 
+                        | JAILHOUSE_MEM_ROOTSHARED,
+
+                },
+
+                /* IVHSMEM  2*/ {
+                        .phys_start = 0x275200000,
+                        .virt_start = 0x275200000,
+                        .size = 0x1000,
+                        .flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE 
+                         | JAILHOUSE_MEM_ROOTSHARED, 
+                },
 	},
+
+	.irqchips = { 
+		/* GIC */
+		{
+			.address = 0x03881000,
+			.pin_base = 288,
+			.pin_bitmap = {
+				0,
+				3 << (332 - 320) /* irq 332 and 333 */
+			},
+		},
+	},
+
+	/* FIXME: add irqchips section */
+	.pci_devices = {
+                {
+                        .type = JAILHOUSE_PCI_TYPE_IVSHMEM,
+                        .bdf = 0x0 << 3,
+                        .bar_mask = {
+                                0xffffff00, 0xffffffff, 0x00000000,
+                                0x00000000, 0x00000000, 0x00000000,
+                        },
+
+                        /*num_msix_vectors needs to be 0 for INTx operation*/
+                        .num_msix_vectors = 0,
+                        .shmem_region = 2,
+                        .shmem_protocol = JAILHOUSE_SHMEM_PROTO_UNDEFINED,
+                        .domain = 0x0,
+                },
+
+                {
+                        .type = JAILHOUSE_PCI_TYPE_IVSHMEM,
+                        .bdf = 0xf << 3,
+                        .bar_mask = {
+                                0xffffff00, 0xffffffff, 0x00000000,
+                                0x00000000, 0x00000000, 0x00000000,
+                        },
+
+                        /*num_msix_vectors needs to be 0 for INTx operation*/
+                        .num_msix_vectors = 0,
+                        .shmem_region = 3,
+                        .shmem_protocol = JAILHOUSE_SHMEM_PROTO_UNDEFINED,
+			.domain = 0x0,
+                },
+        },
+
 };
